c8d7dcc3345d4bcebcd155633da919aea293264d
Rowin <rgsmith@colgate.edu>
2025-09-29 16:54:14 -0400

    added unit conversion

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ const weatherQueryForm = document.querySelector("#weather-query-form");
 const locationInput = document.querySelector("#location");
 const conditonsDisplay = document.querySelector(".conditions-display");
 const tempDewpDisplay = document.querySelector(".temp-dewp-display");
+const unitToggle = document.querySelector(".unit-toggle");
+
+let celsius = false;
+
+unitToggle.addEventListener("click", () => {
+    celsius = !celsius;
+    if(celsius) {
+        unitToggle.textContent = "Celsius";
+    }
+    else {
+        unitToggle.textContent = "Fahrenheit";
+    }
+});
 
 async function displayWeatherData(location){
     const weatherData = createSimpleWeatherObject(await getWeatherData(location));
@@ -22,11 +35,22 @@ async function handleGetWeather(e) {
 
 weatherQueryForm.addEventListener("submit", handleGetWeather);
 
+const convertToCelsius = (temp) => {
+    return Math.floor((temp - 32) * (5/9));
+}
+
 const createSimpleWeatherObject = (weatherData) => {
 
-  const temp = weatherData.days[0].temp;
-  const dewp = weatherData.days[0].dew;
+    let temp = weatherData.days[0].temp;
+let dewp = weatherData.days[0].dew;
+    if(celsius){
+        temp = convertToCelsius(temp);
+        dewp = convertToCelsius(dewp);
+    }
+
   const conditions = weatherData.days[0].conditions;
+
+
 
   return { temp: temp, dewp: dewp, conditions: conditions };
 }
