9e15b2462cea3132987475b5fd7e7cb788619ef5
Rowin <rgsmith@colgate.edu>
2025-09-29 16:19:07 -0400

    added form simplified JSON processing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import "./styles.css";
-import {getTemperature, getDewpoint, getConditions} from "./weather-api-calls.js";
+import {getWeatherData} from "./weather-api-calls.js";
 
-getTemperature("Hamilton, NY");
-getDewpoint("Hamilton, NY");
-getConditions("Hamilton, NY");
+const weatherQueryForm = document.getElementById("weather-query-form");
+const locationInput = document.getElementById("location");
+
+async function handleGetWeather(e) {
+    e.preventDefault();
+    const location = locationInput.value;
+
+    const weatherData = await getWeatherData(location);
+    console.log(weatherData);
+}
+
+weatherQueryForm.addEventListener("submit", handleGetWeather);
+
+const createSimpleWeatherObject = (weatherData) => {
+    const temp = weatherData.days[0].temp;
+    const dewp = weatherData.days[0].dew;
+    const conditions = weatherData.days[0].conditions;
+
+    return {"temp": temp, "dewp": dewp, "conditions": conditions };
+}
